bb6a214e34a69b076c2340b405c9bf9fa1fee082
Bob_work <musgravehill@gmail.com>
2017-11-05 19:23:25 +0300

    wifi

--- sd-rtc-dht-http-v2/start.lua
+++ sd-rtc-dht-http-v2/start.lua
@@ -1,7 +1,10 @@
 do
-module_wifi
 
+module_wifi = require('module_wifi')
+function wifi_onConnectOk_callback()
+    print("ip now", wifi.sta.getip())
+    module_wifi= nil 
+end
+module_wifi.init(wifi_onConnectOk_callback)    
 
-print("ip now", wifi.sta.getip())   
-
-
+end
