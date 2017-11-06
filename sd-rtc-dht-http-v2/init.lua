6b6309840315551bea7e2fc7e57d72e302a59648
Bob_work <musgravehill@gmail.com>
2017-11-06 14:35:34 +0300

    json

--- sd-rtc-dht-http-v2/init.lua
+++ sd-rtc-dht-http-v2/init.lua
@@ -1,1 +1,6 @@
 dofile('start.lua') 
+
+-- TODO
+-- log webserver request to SD
+-- log to sd if inetrnet fail and ok
+-- log to sd about currency bat\main
--- sd-rtc-dht-http-v2/web_data_json.lua
+++ sd-rtc-dht-http-v2/web_data_json.lua
@@ -11,51 +11,45 @@ local datas = {
     ['reset_reason']=reset_reason,
     ['heapKB']=(node.heap()/1024)
 }
-datas['SD']= {}
-datas['FLASH']= {}
 
+datas['SD']= {}
 if(file.chdir('/SD0')) then      
     datas['SD']['is_ok'] =1                 
-    local remaining, used, total=file.fsinfo()
-    buf = buf..'<h2>SD</h2>'  
-    buf = buf..'<b>Total</b> '..(total / 1024)..' MB<br>'
-    buf = buf..'<b>Used</b> '..(used / 1024)..' MB<br>'
-    buf = buf..'<b>Remain</b> '..(remaining / 1024)..' MB<br><br>'          
+    local remaining, used, total=file.fsinfo()    
+    datas['SD']['Total,MB'] =  total / 1024 
+    datas['SD']['Used,MB'] =   used / 1024 
+    datas['SD']['Remain,MB'] = remaining / 1024 
     local l = file.list()
-    for k,v in pairs(l) do
-        buf = buf..'name:'..k..'  '..(v)..' KB<br>\r\n' 
+    datas['SD']['files']={} 
+    for k,v in pairs(l) do        
+        table.insert(datas['SD']['files'], { ['name']=k, ['size,KB']=(v/1024) })
     end    
 else
    datas['SD']['is_ok'] =0               
-end  
-     
+end   
+
+datas['FLASH']= {}    
+if(file.chdir('/FLASH')) then              
+	datas['FLASH']['is_ok'] =1                 
+    local remaining, used, total=file.fsinfo()    
+    datas['FLASH']['Total,KB'] =  total / 1024 
+    datas['FLASH']['Used,KB'] =   used / 1024 
+    datas['FLASH']['Remain,KB'] = remaining / 1024 
+    local l = file.list()
+    datas['FLASH']['files']={} 
+    for k,v in pairs(l) do        
+        table.insert(datas['FLASH']['files'], { ['name']=k, ['size,KB']=(v/1024) })
+    end      
+else
+	datas['FLASH']['is_ok'] =0                 
+end    
+
 local ok, json = pcall(sjson.encode, datas) --pcall for big table
 if ok then
    buf = buf..json
 else
    buf = buf..'{"error":1}' 
-end
-      
-       
- 
-
-    
-if(file.chdir('/FLASH')) then              
-	local remaining, used, total=file.fsinfo()    
-	buf = buf..'<h2>FLASH</h2>'          
-	buf = buf..'<b>Total</b> '..(total / 1024)..' KB<br>'
-	buf = buf..'<b>Used</b> '..(used / 1024)..' KB<br>'
-	buf = buf..'<b>Remain</b> '..(remaining / 1024)..' KB<br><br>'          
-	local l = file.list()
-	for k,v in pairs(l) do
-	buf = buf..'name:'..k..'  '..(v / 1024)..' KB<br>\r\n' 
-	end    
-else
-	buf = buf..'<h2>FLASH mount FAIL</h2>'                 
-end         
- 
-
-        
+end        
    
 
 return buf
